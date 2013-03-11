3fa606b2a00709edd23003e573dcb4c8d1bd2b5e
Andreas Lefkopoulos <andreas.l@adb.se>
2013-03-11 21:52:20 +0100

    Added a Test-project

---
+++ tddTest.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace tddTest.Tests
+{
+    
+    [TestFixture]
+    public class KontoTests
+    {
+        [Test]
+        public void AFirstTest()
+        {
+            Assert.IsTrue(true, "true is true!");
+        }
+    }
+}
